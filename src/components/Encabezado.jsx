1511193ac5bec561ba8c8aa9f2aedd90b00d3c18
Julio Olivas <somarribajulio532@gmail.com>
2025-06-01 20:24:28 -0600

    Traductor de idioma

--- src/components/Encabezado.jsx
+++ src/components/Encabezado.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -8,25 +8,26 @@ import { useAuth } from "../database/authcontext";
 import logo from "../assets/ferreteria_selva_logo.png";
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import "../App.css";
+import { useTranslation } from 'react-i18next';
+import { NavDropdown } from "react-bootstrap";
 
 const Encabezado = () => {
   const [isCollapsed, setIsCollapsed] = useState(false);
   const { isLoggedIn, logout } = useAuth();
+  const { t, i18n } = useTranslation();
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const cambiarIdioma = (lang) => {
+    i18n.changeLanguage(lang);
+  };
 
   const handleLogout = async () => {
     try {
-      // Cerrar el offcanvas antes de proceder
       setIsCollapsed(false);
-
-      // Eliminar variables almacenadas en localStorage
       localStorage.removeItem("adminEmail");
       localStorage.removeItem("adminPassword");
-
-      // Cerrar sesión
       await logout();
-
-      // Redirigir al inicio
       navigate("/");
     } catch (error) {
       console.error("Error al cerrar sesión:", error);
@@ -35,7 +36,6 @@ const Encabezado = () => {
 
   const handleToggle = () => setIsCollapsed(!isCollapsed);
 
-  // Funciones de navegación
   const handleNavigate = (path) => {
     navigate(path);
     setIsCollapsed(false);
@@ -44,11 +44,17 @@ const Encabezado = () => {
   return (
     <Navbar expand="sm" fixed="top" className="color-navbar">
       <Container>
-        <Navbar.Brand onClick={() => handleNavigate("/inicio")} className="text-white" style={{ cursor: "pointer" }}>
+        <Navbar.Brand
+          onClick={() => handleNavigate("/inicio")}
+          className="text-white"
+          style={{ cursor: "pointer" }}
+        >
           <img alt="" src={logo} width="30" height="30" className="d-inline-block align-top" />{" "}
-          <strong>Proyecto Julio</strong>
+          <strong>{t("Proyecto Julio")}</strong>
         </Navbar.Brand>
+
         <Navbar.Toggle aria-controls="offcanvasNavbar-expand-sm" onClick={handleToggle} />
+
         <Navbar.Offcanvas
           id="offcanvasNavbar-expand-sm"
           aria-labelledby="offcanvasNavbarLabel-expand-sm"
@@ -61,50 +67,79 @@ const Encabezado = () => {
               id="offcanvasNavbarLabel-expand-sm"
               className={isCollapsed ? "color-texto-marca" : "text-white"}
             >
-              Menú
+              {t("menu.tituloMenu")}
             </Offcanvas.Title>
           </Offcanvas.Header>
+
           <Offcanvas.Body>
             <Nav className="justify-content-end flex-grow-1 pe-3">
-
               <Nav.Link
                 onClick={() => handleNavigate("/inicio")}
                 className={isCollapsed ? "color-texto-marca" : "text-white"}
               >
-                {isCollapsed ? <i className="bi-house-door-fill me-2"></i> : null}
-                <strong>Inicio</strong>
+                {isCollapsed && <i className="bi-house-door-fill me-2"></i>}
+                <strong>{t("menu.inicio")}</strong>
               </Nav.Link>
-              {isLoggedIn ? (
-                <>
-                  <Nav.Link onClick={handleLogout} className={isCollapsed ? "text-black" : "text-white"}>
-                    Cerrar Sesión
-                  </Nav.Link>
-                </>
-              ) : location.pathname === "/" && (
-                <Nav.Link
-                  onClick={() => handleNavigate("/")}
-                  className={isCollapsed ? "text-black" : "text-white"}
-                >
-                  Iniciar Sesión
-                </Nav.Link>
-              )}
 
-             <Nav.Link
-                    onClick={() => handleNavigate("/clima")}
-                    className={isCollapsed ? "color-texto-marca" : "text-white"}
-               >
-                   {isCollapsed ? <i className="bi-cloud-sun-fill me-2"></i> : null}
-                  <strong>Clima</strong>
+              <Nav.Link onClick={() => handleNavigate("/categorias")} className="text-white">
+                {t("menu.categorias")}
+              </Nav.Link>
 
+              <Nav.Link onClick={() => handleNavigate("/productos")} className="text-white">
+                {t("menu.productos")}
+              </Nav.Link>
+
+              <Nav.Link onClick={() => handleNavigate("/libros")} className="text-white">
+                {t("menu.libros")}
+              </Nav.Link>
+
+              <Nav.Link onClick={() => handleNavigate("/catalogos")} className="text-white">
+                {t("Catalogos")}
+              </Nav.Link>
+
+              <Nav.Link onClick={() => handleNavigate("/pronunciacion")} className="text-white">
+                {t("menu.pronunciacion")}
+              </Nav.Link>
+
+              <Nav.Link
+                onClick={() => handleNavigate("/clima")}
+                className={isCollapsed ? "color-texto-marca" : "text-white"}
+              >
+                {isCollapsed && <i className="bi-cloud-sun-fill me-2"></i>}
+                <strong>{t("menu.clima")}</strong>
               </Nav.Link>
 
               <Nav.Link onClick={() => handleNavigate("/estadisticas")} className="text-white">
-            Estadisticas
-          </Nav.Link>
+                {t("menu.estadisticas")}
+              </Nav.Link>
 
-              
+              {isLoggedIn ? (
+                <Nav.Link onClick={handleLogout} className="text-white">
+                  {t("menu.cerrarSesion")}
+                </Nav.Link>
+              ) : location.pathname === "/" && (
+                <Nav.Link onClick={() => handleNavigate("/")} className="text-white">
+                  {t("menu.iniciarSesion")}
+                </Nav.Link>
+              )}
 
-
+              <NavDropdown
+                title={
+                  <span>
+                    <i className="bi-translate me-2"></i>
+                    {isCollapsed && <span>{t("menu.idioma")}</span>}
+                  </span>
+                }
+                id="basic-nav-dropdown"
+                className={isCollapsed ? "color-texto-marca" : "texto-blanco"}
+              >
+                <NavDropdown.Item onClick={() => cambiarIdioma("es")} className="text-black">
+                  <strong>{t("menu.español")}</strong>
+                </NavDropdown.Item>
+                <NavDropdown.Item onClick={() => cambiarIdioma("en")} className="text-black">
+                  <strong>{t("menu.ingles")}</strong>
+                </NavDropdown.Item>
+              </NavDropdown>
             </Nav>
           </Offcanvas.Body>
         </Navbar.Offcanvas>
--- src/views/Inicio.jsx
+++ src/views/Inicio.jsx
@@ -1,78 +1,52 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from "react-router-dom";
 import { Container, Button } from "react-bootstrap";
 import ModalInstalacionIOS from "../components/inicio/ModalInstalacionIOS";
 
 const Inicio = () => {
+  const [solicitudInstalacion, setSolicitudInstalacion] = useState(null);
+  const [mostrarBotonInstalacion, setMostrarBotonInstalacion] = useState(false);
+  const [esDispositivoIOS, setEsDispositivoIOS] = useState(false);
+  const [mostrarModalInstrucciones, setMostrarModalInstrucciones] = useState(false);
 
-    const navigate = useNavigate();
+  useEffect(() => {
+    const esIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+    setEsDispositivoIOS(esIOS);
+  }, []);
 
-    // Variables de estado para la instalación de la PWA y detección de iOS
-    const [solicitudInstalacion, setSolicitudInstalacion] = useState(null);
-    const [mostrarBotonInstalacion, setMostrarBotonInstalacion] = useState(false);
-    const [esDispositivoIOS, setEsDispositivoIOS] = useState(false);
-    const [mostrarModalInstrucciones, setMostrarModalInstrucciones] = useState(false);
-
-    // Detectar dispositivo iOS
-    useEffect(() => {
-        const esIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-        setEsDispositivoIOS(esIOS);
-    }, []);
-
-    // Manejar evento beforeinstallprompt
-    useEffect(() => {
-        const manejarSolicitudInstalacion = (evento) => {
-            evento.preventDefault();
-            setSolicitudInstalacion(evento);
-            setMostrarBotonInstalacion(true);
-        };
-
-        window.addEventListener("beforeinstallprompt", manejarSolicitudInstalacion);
-
-        return () => {
-            window.removeEventListener("beforeinstallprompt", manejarSolicitudInstalacion);
-        };
-    }, []);
-
-    // Función de navegación
-    const handleNavigate = (path) => {
-      navigate(path);
+  useEffect(() => {
+    const manejarSolicitudInstalacion = (evento) => {
+      evento.preventDefault();
+      setSolicitudInstalacion(evento);
+      setMostrarBotonInstalacion(true);
     };
 
-    // Función para disparar el prompt de instalación
-    const instalacion = async () => {
-        if (!solicitudInstalacion) return;
-        try {
-            await solicitudInstalacion.prompt();
-            const { outcome } = await solicitudInstalacion.userChoice;
-            console.log(outcome === "accepted" ? "Instalación aceptada" : "Instalación rechazada");
-        } catch (error) {
-            console.error("Error al intentar instalar la PWA:", error);
-        } finally {
-            setSolicitudInstalacion(null);
-            setMostrarBotonInstalacion(false);
-        }
+    window.addEventListener("beforeinstallprompt", manejarSolicitudInstalacion);
+    return () => {
+      window.removeEventListener("beforeinstallprompt", manejarSolicitudInstalacion);
     };
+  }, []);
 
-    console.log("esDispositivoIOS:", esDispositivoIOS);
-console.log("mostrarBotonInstalacion:", mostrarBotonInstalacion);
-console.log("solicitudInstalacion:", solicitudInstalacion);
+  const instalacion = async () => {
+    if (!solicitudInstalacion) return;
+    try {
+      await solicitudInstalacion.prompt();
+      const { outcome } = await solicitudInstalacion.userChoice;
+      console.log(outcome === "accepted" ? "Instalación aceptada" : "Instalación rechazada");
+    } catch (error) {
+      console.error("Error al intentar instalar la PWA:", error);
+    } finally {
+      setSolicitudInstalacion(null);
+      setMostrarBotonInstalacion(false);
+    }
+  };
 
-    // Funciones para mostrar y ocultar el modal de instrucciones para iOS
-    const abrirModalInstrucciones = () => setMostrarModalInstrucciones(true);
-    const cerrarModalInstrucciones = () => setMostrarModalInstrucciones(false);
+  const abrirModalInstrucciones = () => setMostrarModalInstrucciones(true);
+  const cerrarModalInstrucciones = () => setMostrarModalInstrucciones(false);
 
   return (
     <Container>
-      <h1>Inicio</h1>
-      <button onClick={() => handleNavigate("/categorias")} >Ir a Categorías</button>
-      <button onClick={() => handleNavigate("/productos")} >Ir a Productos</button>
-      <button onClick={() => handleNavigate("/libros")} >Ir a Libros</button>
-      <button onClick={() => handleNavigate("/catalogos")} >Ir a Catalogos</button>
-      <button onClick={() => handleNavigate("/pronunciacion")} >Ir a pronunciacion</button>
-      <br />
       {!esDispositivoIOS && mostrarBotonInstalacion && (
-        <div className="my-4">
+        <div className="my-4 text-center">
           <Button className="sombra" variant="primary" onClick={instalacion}>
             Instalar app Proyecto Julio <i className="bi bi-download"></i>
           </Button>
@@ -91,6 +65,6 @@ console.log("solicitudInstalacion:", solicitudInstalacion);
       />
     </Container>
   );
-}
+};
 
 export default Inicio;
